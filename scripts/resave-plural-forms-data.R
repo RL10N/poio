e5611ea22ce0d3bdba501440d7c25c4f4c061039
richierocks <richierocks@gmail.com>
2016-10-08 10:52:51 +0300

    resave plural forms data, fixing encoding and Macedonian value

--- scripts/resave-plural-forms-data.R
+++ scripts/resave-plural-forms-data.R
@@ -4,7 +4,11 @@
 # And was converted to CSV by Thomas Leeper.
 # Then migrated from the msgtools package by Richie Cotton.
 
-plural_forms <- read.csv("inst/scripts/PluralFormHeaders.csv", stringsAsFactors = FALSE)
+plural_forms <- data.table::fread(
+  "scripts/PluralFormHeaders.csv",
+  stringsAsFactors = FALSE,
+  encoding = "UTF-8"
+)
 
 # There are footnote links from the original webpage that need removing.
 plural_forms$EnglishName[plural_forms$EnglishName == "Arabic [1]"] <- "Arabic"
@@ -13,5 +17,9 @@ plural_forms$EnglishName[plural_forms$EnglishName == "Chinese [2]"] <- "Chinese"
 # For the purposes of auto-replacing the plural form, we can deal with the
 # special case of Mandarin Chinese in code rather than keeping it in the dataset
 plural_forms <- plural_forms[plural_forms$EnglishName != "Chinese [3]", ]
+
+# Macedonian has some additional text in the plural forms description
+plural_forms$PluralFormHeader[plural_forms$EnglishName == "Macedonian"] <- "nplurals=2; plural=(n==1 || n%10==1 ? 0 : 1);"
+
 
 save(plural_forms, file = "data/plural_forms.RData")
