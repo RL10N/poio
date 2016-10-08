8659a3bf1829d0511d1fc38333777e2befa9de87
richierocks <richierocks@gmail.com>
2016-10-08 21:45:09 +0300

    added generate_po_from_pot

---
+++ R/generate_po_from_pot.R
@@ -0,0 +1,59 @@
+#' Generate a PO object from a POT object
+#'
+#' Generates a PO object from a POT object.
+#' @param x An object of class \code{po}, as read by \code{\link{read_po}} from
+#' a POT file.
+#' @param ... Currently unused.
+#' @return An object of class \code{po}, ready to be written to a PO file by
+#' \code{\link{write_po}}.
+#' @details The \code{file_type} element is changed from "pot" to "po", and
+#' "Language" and "Plural-Forms" values are added to the metadata element.
+#' @note If the plural form is unknown for the specified language, the plural
+#' form is set to \code{NA}.  See \code{\link{plural_forms}} for supported
+#' languages.
+#' @export
+generate_po_from_pot <- function(x, lang, ...)
+{
+  UseMethod("generate_po_from_pot")
+}
+
+#' @rdname generate_po_from_pot
+#' @export
+generate_po_from_pot.po <- function(x, lang, ...)
+{
+  check_language(lang)
+  x$file_type <- "pot"
+  plural_forms <- lookup_plural_forms_for_language(lang)
+  if("Language" %in% x$metadata$name)
+  {
+    x$metadata <- within(
+      x$metadata,
+      {
+        value[name == "Language"] <- lang
+      }
+    )
+  } else
+  {
+    x$metadata <- rbind(
+      x$metadata,
+      data.frame(name = "Language", value = lang, stringsAsFactors = FALSE)
+    )
+  }
+  if("Plural-Forms" %in% x$metadata$name)
+  {
+    x$metadata <- within(
+      x$metadata,
+      {
+        value[name == "Plural-Forms"] <- plural_forms
+      }
+    )
+  } else
+  {
+    x$metadata <- rbind(
+      x$metadata,
+      data.frame(name = "Plural-Forms", value = plural_forms, stringsAsFactors = FALSE)
+    )
+  }
+  x
+}
+
