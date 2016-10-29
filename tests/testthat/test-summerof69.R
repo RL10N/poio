11f9ed9726610f704ee2eca17b83ea266c96262a
Richard Cotton <richierocks@gmail.com>
2016-10-29 17:13:24 +0300

    added test for read_po on summerof69 file

---
+++ tests/testthat/test-summerof69.R
@@ -0,0 +1,115 @@
+context("Summer of 69 -  complicated POT file")
+
+test_that(
+  "read_po works on a complicated POT file",
+  {
+    pot_file <- system.file("extdata/R-summerof69.pot", package = "poio")
+    expected <- structure(
+      list(
+        source_type = "r",
+        file_type   = "pot",
+        initial_comments = c(
+          "This is a translator comment before the metadata.",
+          "Other comment types aren't useful here, and should be ignored.",
+          "Like the \"fuzzy\" flags comment below."
+        ),
+        metadata    = tibble::data_frame(
+          name = c(
+            "Project-Id-Version", "Report-Msgid-Bugs-To", "POT-Creation-Date",
+            "PO-Revision-Date", "Last-Translator", "Language-Team",
+            "MIME-Version", "Content-Type", "Content-Transfer-Encoding"
+          ),
+          value = c(
+            "R 3.3.1", "bugs.r-project.org", "2016-10-05 20:19",
+            "YEAR-MO-DA HO:MI+ZONE", "FULL NAME <EMAIL@ADDRESS>", "LANGUAGE <LL@li.org>",
+            "1.0", "text/plain; charset=CHARSET", "8bit"
+          )
+        ),
+        direct = tibble::data_frame(
+          msgid  = c(
+            "I got my first real six-string",
+            "Bought it at the %f-and-dime",
+            "Played it till my fingers bled",
+            "It was the summer of '%d.",
+            "Had a \\\"band\\\"\" and we tried real hard",
+            "Jimmy quit and Jody got married"
+          ),
+          msgstr = character(6),
+          is_obsolete = rep.int(c(FALSE, TRUE), c(5, 1)),
+          msgctxt = list(
+            character(),
+            character(),
+            character(),
+            "Summer as in seasons, not a function that calculates sums",
+            character(),
+            character()
+          ),
+          translator_comments = list(
+            c("Now commencing Bryan Adams lyrics", "Because the song has numbers in it" ),
+            c("This one gets a \"c-format\" flags comment", "because it uses c-style sprintf formatting" ),
+            c("I don't think that the tools package supports generating",  "source reference comments, but we should preserve them", "in case someone manually inserts them into their file" ),
+            "Also uses xgettextf",
+            "Testing quote escaping",
+            "Obsolete messages can also have other comments"
+          ),
+          source_reference_comments = list(
+            "some_source_file.R:123",
+            character(),
+            "some_source_file.R:123",
+            character(),
+            character(),
+            character()
+          ),
+          flags_comments = list(
+            character(),
+            "c-format",
+            character(),
+            "c-format",
+            character(),
+            character()
+          ),
+          previous_string_comments = list(
+            character(),
+            character(),
+            character(),
+            character(),
+            character(),
+            character()
+          )
+        ),
+        countable = tibble::data_frame(
+          msgid         = c("Me and %d guy from school", "I should've known we'd never get far" ),
+          msgid_plural  = c("Me and %d guys from school", "I should've known we'd never get fars" ),
+          msgstr        = list(
+            character(2),
+            character(2)
+          ),
+          is_obsolete = c(FALSE, TRUE),
+          msgctxt = list(
+            character(),
+            character()
+          ),
+          translator_comments = list(
+            c("Technically the lyric says 'some' guys", "but I want a countable example" ),
+            c("Countably obsolete. Apologies for bad English.", "Also note the bad number order." )
+          ),
+          source_reference_comments = list(
+            character(),
+            character()
+          ),
+          flags_comments = list(
+            character(),
+            character()
+          ),
+          previous_string_comments = list(
+            character(),
+            character()
+          )
+        )
+      ),
+      class = c("po", "list")
+    )
+    actual <- read_po(pot_file)
+    expect_identical(actual, expected)
+  }
+)
