0c7bb239b6c1c21e72b22137c5b494b44d943df9
Richard Cotton <richierocks@gmail.com>
2017-01-02 14:56:40 -0500

    alternate simple print

--- R/print.R
+++ R/print.R
@@ -16,23 +16,27 @@ print.po <- function(x, ...)
 {
   cat(
     sprintf(
-      "A %s translation object containing %d direct messages and %d countable messages from %s code.\n",
+      "A %s translation object from %s code\n  containing %d direct messages and %d countable messages.\n",
       switch(x$file_type, pot = "master", po = "language-specific"),
+      x$source_type,
       nrow(x$direct),
-      nrow(x$countable),
-      x$source_type
+      nrow(x$countable)
     )
   )
-  if(length(x$initial_comments) > 0L)
-  {
-    cat("\nInitial comments:\n")
-    cat(x$initial_comments, sep = "\n")
-  }
-  cat("\nMetadata:\n")
-  print(x$metadata, ...)
+  cat("\nSelected Metadata:\n")
+  x$metadata %>%
+    filter_(~ name %in% c("Project-Id-Version", "PO-Revision-Date", "Last-Translator")) %>%
+    as.data.frame() %>%
+    print(...)
   cat("\nDirect Translations:\n")
-  print(x$direct, ...)
+  x$direct %>%
+    select_(~ msgid, ~ msgstr) %>%
+    as.data.frame() %>%
+    print(...)
   cat("\nCountable Translations:\n")
-  print(x$countable, ...)
+  x$countable %>%
+    select_(~ msgid, ~ msgid_plural, ~ msgstr) %>%
+    as.data.frame() %>%
+    print(...)
   invisible(x)
 }
