228cf6017e24db415483aa955baf3f21c22f534c
Richard Cotton <richierocks@gmail.com>
2017-01-09 20:44:00 -0500

    custom error finding when using expect_equal on 2 data frames

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,7 +4,8 @@ library(poio)
 # Can't compare po object directly using
 # expect_identical(po1, po2) due to
 # https://github.com/hadley/dplyr/issues/2194
-# Use this instead
+# testthat also not informative on where differences in data_frames are
+# so roll custom solution.
 
 expect_po_equal <- function(actual, expected, check_po_revision_date = FALSE) {
   expect_identical(actual$source_type, expected$source_type)
@@ -38,13 +39,35 @@ expect_po_equal <- function(actual, expected, check_po_revision_date = FALSE) {
   # to data.frames before comparing
   actual_metadata <- as.data.frame(actual$metadata)
   expected_metadata <- as.data.frame(expected$metadata)
-  expect_equal(actual_metadata, expected_metadata)
+  metadata_info <- get_difference_info(actual$metadata, expected$metadata)
+  expect_equal(actual_metadata, expected_metadata, info = metadata_info)
   actual_direct <- as.data.frame(actual$direct)
   expected_direct <- as.data.frame(expected$direct)
-  expect_equal(actual_direct, expected_direct)
+  direct_info <- get_difference_info(actual$direct, expected$direct)
+  expect_equal(actual_direct, expected_direct, info = direct_info)
   actual_countable <- as.data.frame(actual$countable)
   expected_countable <- as.data.frame(expected$countable)
-  expect_equal(actual_countable, expected_countable)
+  countable_info <- get_difference_info(actual$countable, expected$countable)
+  expect_equal(actual_countable, expected_countable, info = countable_info)
+}
+
+flatten <- function(d) {
+  d[] <- lapply(
+    colnames(d),
+    function(colname) {
+      vapply(d[[colname]], toString, character(1))
+    }
+  )
+  d
+}
+
+get_difference_info <- function(d1, d2) {
+  fd1 <- flatten(d1)
+  fd2 <- flatten(d2)
+  diffs12 <- dplyr::anti_join(fd1, fd2)
+  diffs21 <- dplyr::anti_join(fd2, fd1)
+  diffs_both <- cbind(actual = diffs12, expected = diffs21)
+  assertive.base::print_and_capture(diffs_both)
 }
 
 test_check("poio")
